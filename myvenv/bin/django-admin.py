bc516f10fc68276855d3f704ab80a37a3b9bd536
hyeonJeongByeon <jungjung1218@gmail.com>
2019-07-30 13:12:41 -0400

    comment

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/byeonhyeonjeong/Downloads/login_session-master/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
